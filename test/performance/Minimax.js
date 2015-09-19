2a54e156bc436e1d72eac39362f823eea6f90476
James Pickard <james.pickard@gmail.com>
2015-09-19 14:19:32 -0400

    Fix perf test

--- test/performance/Minimax.js
+++ test/performance/Minimax.js
@@ -1,9 +1,17 @@
 import Minimax from '../../js/hexbusters/Minimax.js';
 import InitialState from '../../js/constants/InitialState.js';
+import { COLOUR_RED } from '../../js/constants/Colours.js';
 
 const gameState = {
   ...InitialState,
   players: [{colour: 'red'}, {colour: 'blue'}]
+};
+
+const gridSettings = {
+  width: 3,
+  height: 3,
+  orientation: 'flat-topped',
+  layout: 'odd-q'
 };
 
 const timeLimitMs = 5000;
@@ -11,7 +19,9 @@ console.log(`Running Minimax board evaluation for ${timeLimitMs}ms...`);
 
 const startTime = new Date();
 const { statesEvaluated } = Minimax.evaluateState(
+  COLOUR_RED,
   gameState,
+  gridSettings,
   startTime.getTime() + timeLimitMs
 );
 
