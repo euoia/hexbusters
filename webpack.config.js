113d44c904ba15a7424c7463415cb61d95b96d96
James Pickard <james.pickard@gmail.com>
2021-10-02 13:36:19 +0100

    Fix global CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-var path = require("path");
-var webpack = require("webpack");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: ["./js/app.js"],
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: ['./js/app.js'],
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist'
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "HEXBUSTERS • A board game",
-    }),
+      title: 'HEXBUSTERS • A board game'
+    })
   ],
   module: {
     rules: [
-      { test: /\.js|\.jsx$/, use: "babel-loader" },
+      { test: /\.js|\.jsx$/, use: 'babel-loader' },
       {
         test: /\.css$/i,
-        use: ["css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
               // Don't want ES modules because we need to dynamically require images.
-              esModule: false,
-            },
-          },
-        ],
-      },
-    ],
-  },
+              esModule: false
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
